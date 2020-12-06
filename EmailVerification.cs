ef9ba625870623797d4e3fd2dd3d8fa243fe323e
Avenger2627 <imraninfo97@gmail.com>
2020-12-06 16:09:04 +0530

    [Imran Shaikh] Add: Validte Email ID

---
+++ EmailVerification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Email
+{
+    class EmailVerification
+    {
+        public static string EmailID = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.,-]+$";
+        public bool Emails(string Email)
+        {
+            Regex Em = new Regex(EmailID);
+            bool Validate = Em.IsMatch(Email);
+            return Validate;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace Email
         static void Main(string[] args)
         {
             Console.WriteLine("***************Welcome to Email Verification Problem*************");
+            EmailVerification Email = new EmailVerification();
+            Console.Write("Enter Email_ID : ");
+            string Emailid = Console.ReadLine();
+            Console.WriteLine(Email.Emails(Emailid));
         }
     }
 }
